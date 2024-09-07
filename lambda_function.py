377fbae7152172ac0af7d602732edb609ee386f5
Italo Ventura <itozinventura132@gmail.com>
2024-09-07 19:46:36 -0300

    Adding modifications to code

--- lambda_function.py
+++ lambda_function.py
@@ -6,6 +6,7 @@ def lambda_handler(event, context):
     # TODO implement
     print(event)
     minha_var = os.environ['MINHA_VAR']
+    print("Testando config de variável de ambiente lambda AWS", minha_var)
     l.log_function("Log de Execução após configurar Github action " + event)
 
     return {
