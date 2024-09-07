17fa7d8d90ea6d1efae0f339e99a04a46b6bd8a5
Italo Ventura <itozinventura132@gmail.com>
2024-09-07 19:44:38 -0300

    Adding modifications to code

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,11 @@
 import json
 import log as l
+import os
 
 def lambda_handler(event, context):
     # TODO implement
     print(event)
-
+    minha_var = os.environ['MINHA_VAR']
     l.log_function("Log de Execução após configurar Github action " + event)
 
     return {
